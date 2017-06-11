fde487459aa92b0d98aa195d5f88c91e8fc85ff3
Chick <cheecken0@hotmail.com>
2017-06-11 13:10:13 +0800

    Add project files.

---
+++ SQLTest2/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace SQLTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputName = new System.Windows.Forms.TextBox();
+            this.inputAge = new System.Windows.Forms.TextBox();
+            this.inputMoney = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonAddData = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputName
+            // 
+            this.inputName.Location = new System.Drawing.Point(24, 56);
+            this.inputName.Name = "inputName";
+            this.inputName.Size = new System.Drawing.Size(100, 20);
+            this.inputName.TabIndex = 0;
+            // 
+            // inputAge
+            // 
+            this.inputAge.Location = new System.Drawing.Point(24, 107);
+            this.inputAge.Name = "inputAge";
+            this.inputAge.Size = new System.Drawing.Size(100, 20);
+            this.inputAge.TabIndex = 0;
+            // 
+            // inputMoney
+            // 
+            this.inputMoney.Location = new System.Drawing.Point(24, 164);
+            this.inputMoney.Name = "inputMoney";
+            this.inputMoney.Size = new System.Drawing.Size(100, 20);
+            this.inputMoney.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Money";
+            // 
+            // buttonAddData
+            // 
+            this.buttonAddData.Location = new System.Drawing.Point(137, 105);
+            this.buttonAddData.Name = "buttonAddData";
+            this.buttonAddData.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddData.TabIndex = 2;
+            this.buttonAddData.Text = "Add data";
+            this.buttonAddData.UseVisualStyleBackColor = true;
+            this.buttonAddData.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(237, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(87, 128);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 211);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonAddData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputMoney);
+            this.Controls.Add(this.inputAge);
+            this.Controls.Add(this.inputName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputName;
+        private System.Windows.Forms.TextBox inputAge;
+        private System.Windows.Forms.TextBox inputMoney;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonAddData;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SQLTest2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace SQLTest2
+{
+    public struct Person
+    {
+        public string name;
+        public string age;
+        public string money;
+        public string addTime;
+    }
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Connect to server
+            Connect();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //declare new struct of newGuy
+            Person newGuy;
+
+            //declare the members of struct of newGuy
+            newGuy.name = inputName.Text;
+            newGuy.age = inputAge.Text;
+            newGuy.money = inputMoney.Text;
+            newGuy.addTime = GetTimestamp(DateTime.Now);
+
+            //Add text to Database
+        }
+
+        public void Connect()
+        {
+            //Connects to server somehow
+        }
+
+        public void AddToSQL(Person c)
+        {
+            //Adds to database somehow
+        }
+
+        private string GetTimestamp(DateTime value)
+        {
+            return value.ToString("yyyyMMddHHmmssfff");
+        }
+
+    }
+}
